40c3344daba5828fa2a48f52ece81b853be49f48
Ignatius-code1 <igidydabbhs@gmail.com>
2025-07-29 23:36:21 +0300

    add

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -3,14 +3,17 @@ import { useParams } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 function Movie() {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState({ genres: [] });
   const { id } = useParams();
 
   useEffect(() => {
     fetch(`http://localhost:4000/movies/${id}`)
       .then(r => r.json())
-      .then(data => setMovie(data))
-      .catch(err => console.error("Failed to fetch movie:", err));
+      .then(data => setMovie(data || { genres: [] }))
+      .catch(err => {
+        console.error("Failed to fetch movie:", err);
+        setMovie({ genres: [] });
+      });
   }, [id]);
 
   return (
